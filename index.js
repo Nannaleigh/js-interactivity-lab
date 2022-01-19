aa3001c4ce2d8514f31fca4317adfa6a6e5cdbb8
Nannaleigh <tatianna.martinez71@gmail.com>
2022-01-19 14:19:38 -0700

    Core Lab Completed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-console.log('Hello World!')
+
+
+const message = document.querySelector("#message")
 
 function addMovie(event) {
     event.preventDefault()
@@ -8,14 +10,30 @@ const movie = document.createElement('li');
 
 const movieTitle = document.createElement('span');
 movieTitle.textContent = inputField.value;
+movieTitle.addEventListener("click", crossOffMovie)
 movie.appendChild(movieTitle)
 
 const deleteBtn = document.createElement("button");
 deleteBtn.textContent = "X"
-deleteBtn.addEventListener("click",)
-movie.appendChild(deleteBtn)
+deleteBtn.addEventListener("click", deleteMovie)
+movie.appendChild(deleteBtn);
+
 document.querySelector("ul").appendChild(movie);
 inputField.value = " "
 };
 
 document.querySelector("form").addEventListener("submit", addMovie)
+
+function deleteMovie(event) {
+    event.target.parentNode.remove();
+    message.textContent = "Movie deleted!"
+};
+
+function crossOffMovie(event) {
+    event.target.classList.toggle("checked") 
+    if (event.target.classList.contains("checked") === true ) {
+        message.textContent = "Movie Watched!"
+    } else {
+        message.textContent = "Movie added back!"
+    }  
+}
